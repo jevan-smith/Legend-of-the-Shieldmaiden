91d8876ff0e63eca114634880ef503ba13403a24
jevan1000 <smithjev@sonoma.edu>
2018-03-31 00:31:40 -0700

    tilemap/camera moving
    added a camera that moves with the character, and some game tiles.

---
+++ sheildmaiden/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    private Vector2 volocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref volocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref volocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+
+}
