15293501dfabbf571a2637a4bde6d023bc76d004
SeanMilty95 <36428443+SeanMilty95@users.noreply.github.com>
2018-04-24 22:21:42 -0700

    Added Exploding Blob
    -Bat has "attack speed"
    -Added Blob
    -Blob will blink red and stop moving if explosion trigger activated.
    -Blob will explode
    -Dmg radius set but doesn't yet dmg player.

---
+++ sheildmaiden/Assets/Scripts/Enemy Scripts/Blob/Blob_At_Trig.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Blob_At_Trig : MonoBehaviour
+{
+
+    //public GameObject Parent;
+    private Animator motion;
+
+    private Color red = Color.red;
+
+    private void Start()
+    { 
+        
+        motion = GetComponentInParent<Animator>();//Sets motion to parent game object animator component.
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")//Checks for PLayer
+        {
+            /* **Make Explode** */
+            GetComponentInParent<BlobAI>().Exploding = true;
+
+            //new WaitForSeconds(.4f);
+            StartCoroutine(turnRed());
+            motion.SetBool("Explosion", true);
+                
+            GetComponentInParent<BlobAI>().curr_hp = 0;
+            new WaitForSeconds(.2f);
+            GetComponentInParent<BlobAI>().Exploded = true;
+            GetComponentInParent<BlobAI>().Exploding = false;
+            
+        }
+
+    }
+    void redSprite()
+    {
+        GetComponentInParent<BlobAI>().myRenderer.material.shader = GetComponentInParent<BlobAI>().shaderGUItext;
+        GetComponentInParent<BlobAI>().myRenderer.color = red;
+    }
+
+    void normalSprite()
+    {
+        GetComponentInParent<BlobAI>().myRenderer.material.shader = GetComponentInParent<BlobAI>().shaderSpritesDefault;
+        GetComponentInParent<BlobAI>().myRenderer.color = Color.white;
+    }
+
+    public IEnumerator turnRed()
+    { 
+        for (int i = 0; i < 5; i++)
+        {
+            redSprite();
+            yield return new WaitForSeconds(.1f);
+            normalSprite();
+            yield return new WaitForSeconds(.1f);
+        }
+        
+    }
+
+}
+
---
+++ sheildmaiden/Assets/Scripts/Enemy Scripts/Blob/Blob_On_Hit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob_On_Hit : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Weapon")//Checks for weapon hit
+        {
+            if (this.transform.parent.GetComponent<BlobAI>().curr_hp != 0)//gets variable from main script
+            {
+                this.transform.parent.GetComponent<BlobAI>().curr_hp -= GameObject.Find("Player").GetComponent<Player>().Player_Damage;//changes variable from parent object
+                this.transform.parent.GetComponent<BlobAI>().blink = true;
+                this.transform.parent.GetComponent<BlobAI>().hit_sound = true;
+            }
+        }
+    }
+}
