f8286bf73916f98f6ef2f4fc5ff2e77f52b302c9
e102y <e102y@users.noreply.github.com>
2018-05-06 19:43:39 -0700

    Improved Title Screen Scaling

--- sheildmaiden/Assets/Scripts/Start Screen/AnyKeyStart.cs
+++ sheildmaiden/Assets/Scripts/Start Screen/AnyKeyStart.cs
@@ -7,6 +7,12 @@ using UnityEngine.UI;
 public class AnyKeyStart : MonoBehaviour {
 
 	[SerializeField] private Text textToUse;
+	public Texture2D fadeOutTexture; // black image that will overlay the screen
+	public float fadeSpeed = 0.8f;
+	private int drawDepth = -1000;
+	private float alpha = 1.0f;
+	private int fadeDir = -1;
+
 	// Use this for initialization
 	void Start () {
 		textToUse = GetComponent<Text>();
@@ -21,5 +27,27 @@ public class AnyKeyStart : MonoBehaviour {
 			textToUse.text = "Loading...";
 			SceneManager.LoadScene("NeverUnload", 0);
 		}
+	}
+
+
+	void OnGUI()
+	{
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01(alpha);
+
+		GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+	}
+
+	public float BeginFade (int direction)
+	{
+		fadeDir = direction;
+		return (fadeSpeed);
+	}
+
+	private void OnLevelWasLoaded()
+	{
+		BeginFade(-1);
 	}
 }
