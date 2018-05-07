9f5d21b44bccb5b9224dd69640f221b0b184e961
e102y <e102y@users.noreply.github.com>
2018-05-06 19:01:04 -0700

    Added Title Screen
    added basic title screen functionality, I will try to have an opening scroll done by the end of today

---
+++ sheildmaiden/Assets/Scripts/Start Screen/AnyKeyStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AnyKeyStart : MonoBehaviour {
+
+	[SerializeField] private Text textToUse;
+	// Use this for initialization
+	void Start () {
+		textToUse = GetComponent<Text>();
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.anyKeyDown)
+		{
+			textToUse.text = "Loading...";
+			SceneManager.LoadScene("NeverUnload", 0);
+		}
+	}
+}
